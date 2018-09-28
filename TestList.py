733d477c8a22c53b45428126bbe3ef0399b5bb7e
luckyphill <phillip.j.brown@adelaide.edu.au>
2018-09-28 19:35:09 +0930

    heading home

--- TestList.py
+++ TestList.py
@@ -1,12 +1,21 @@
 ## This file contains all the unit tests to make sure everything works as it is written
 
 import os
+import filecmp
 
 import installer
+
+G_ALL_CODES_FILE = 'all_codes.csv'
 
 PATH_FOR_INSTALLER = '/Users/phillipbrown/StockWatch/testing/'
 RAW_DATA = PATH_FOR_INSTALLER + 'raw_data/'
 STOCK_DATA = PATH_FOR_INSTALLER +'data/'
+COMPARISON_FILES_PATH = PATH_FOR_INSTALLER + 'cmp_files/'
+
+
+COMPARISON_ALL_CODES_FILE =  COMPARISON_FILES_PATH + G_ALL_CODES_FILE
+ALL_CODES_FILE = PATH_FOR_INSTALLER + G_ALL_CODES_FILE
+
 
 #==========================================
 # Use pytest to test things work correctly
@@ -23,11 +32,23 @@ def testSetInstallerPaths():
 
 
 def testGetInitialCodes():
-	## This test check that the latest codes are retrieved correctly
+	## This tests check that the latest codes are retrieved correctly
+	## and stored in the correct location
 	clear_testing_folder()
+
+	all_codes = []
+	with open(self.COMPARISON_ALL_CODES_FILE, 'r') as csvfile:
+		codes_reader = csv.reader(csvfile,delimiter=',')
+		all_codes = list(codes_reader)
 
 	inst = installer.Installer(PATH_FOR_INSTALLER,RAW_DATA)
 	inst.GetInitialCodes()
+	assert os.path.isfile(ALL_CODES_FILE)
+	assert filecmp.cmp(ALL_CODES_FILE,COMPARISON_ALL_CODES_FILE)
+	assert ## assert that the initial codes match the codes in all_codes
+
+def 
+
 
 
 
--- installer.py
+++ installer.py
@@ -17,12 +17,14 @@
 
 import os
 import csv
+G_ALL_CODES_FILE = 'all_codes.csv'
 
 class Installer:
 	def __init__(self,path_inst_dir,path_raw_data):
 		self.PATH_TO_INSTALL_DIRECTORY = path_inst_dir
 		self.RAW_PATH = path_raw_data
-		self.ALL_CODES_FILE = self.PATH_TO_INSTALL_DIRECTORY + 'all_codes.csv'
+		self.ALL_CODES_FILE = self.PATH_TO_INSTALL_DIRECTORY + G_ALL_CODES_FILE
+		self.initial_codes = []
 
 	def install(self):
 		## Performs the installation
@@ -40,7 +42,6 @@ class Installer:
 
 	def GetInitialCodes(self):
 		## Reads the most recent of the raw_data files and grabs the stock codes
-		all_codes = []
 
 		latest_file = os.listdir(self.RAW_PATH)[0]
 		for file in os.listdir(self.RAW_PATH)[1:]:
@@ -52,11 +53,52 @@ class Installer:
 		with open(path_to_latest_file, 'rU') as csvfile:
 			code_reader = csv.reader(csvfile, dialect='excel')
 			for code in code_reader:
-				all_codes.append(code[0])
+				self.initial_codes.append(code[0])
 
 		with open(self.ALL_CODES_FILE,'w') as file:
-			for code in all_codes:
+			for code in self.initial_codes:
 				file.write(code + '\n')
+
+	
+	def CleanRawData(self):
+		## This will take the raw data, and put it in the correct format
+		## It will create the file structure as well
+
+		assert self.initial_codes ## Can't run this unless codes GetInitialCodes() has run first
+
+		##===============================================================
+
+		stockPriceData = []
+		latestYear = dt.date.today().year() + 1
+		dates = []
+		for year in xrange(EARLIEST_YEAR, latestYear):
+			for month in xrange(1,13):
+				for day in xrange(1,32):
+					dates.append(year * 10000 + month * 100 + day)
+
+		for date in dates:
+			file_name = RAW_PATH + str(date) + ".txt"
+			if os.path.isfile(file_name):
+				print "Reading data from " + str(date)
+				with open(file_name, 'r') as file:
+					data_reader = csv.reader(file, dialect='excel')
+					for row in data_reader:
+						if row[0] not in stockPriceData and row[0] in codes:
+							stockPriceData[row[0]] = []
+						stockPriceData[row[0]].append(row[1:])
+
+		# save the data into a csv file
+		# make the folder if it doesn't exist
+		if not os.path.exists(STOCK_PATH):
+			os.makedirs(STOCK_PATH)
+				
+		for code in stockPriceData:
+			file_name = STOCK_PATH + code + ".csv"
+			LOG.write(str(dt.datetime.now()) + " Writing data for " + code + "\n")
+			with open(file_name, 'wb') as csvfile:
+				writer = csv.writer(csvfile, delimiter=',')
+				for row in stockPriceData[code]:
+					writer.writerow(row)
 
 
 
